bf1fa02ce828c89f5eb49694d3e682844df76d0a
Wladimir Palant <trev@adblockplus.org>
2012-09-12 08:09:40 +0200

    Simplified build script now that buildtools are guaranteed to exist

--- build.py
+++ build.py
@@ -3,8 +3,5 @@
 
 import os, sys, subprocess
 
-if not os.path.exists('buildtools'):
-  subprocess.Popen(['hg', 'clone', 'https://hg.adblockplus.org/buildtools/']).communicate()
-
 import buildtools.build
 buildtools.build.processArgs('.', sys.argv)
