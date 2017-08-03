d528686235fdd732070560fd2d9fb637d8d55c29
Wladimir Palant <trev@adblockplus.org>
2017-08-03 12:54:08 +0200

    Updated tests, instant search is gone on Google and with it the keyboard navigation

--- tests/google_search.py
+++ tests/google_search.py
@@ -56,12 +56,6 @@ def run(driver):
     driver.wait_until(lambda: init_results(driver))
     assert_link_unchanged()
 
-    # Keyboard navigation
-    search_field = driver.find_element('name', 'q')
-    search_field.click()
-    search_field.send_keys(driver.keys.TAB, driver.keys.TAB, driver.keys.DOWN)
-    assert driver.execute_script('return document.activeElement == arguments[0]', [result])
-
     # Press Enter on a search result
     with driver.restore_url():
         assert_no_intermediate_urls(driver,
@@ -73,26 +67,4 @@ def run(driver):
     # Middle-click search result
     assert_no_intermediate_urls(driver, lambda: result.middle_click(), href)
     driver.close_background_tabs()
-    assert_link_unchanged()
-
-    # Click Apps button to bring up dropdown
-    driver.find_element('css selector', 'a[title="Google apps"]').click()
-    driver.wait_until(lambda: driver.find_element('css selector', 'div[aria-label="Google apps"]').is_displayed())
-    driver.find_element('css selector', 'a[title="Google apps"]').click()
-
-    # Switch off Instant results
-    driver.find_element('id', 'abar_button_opt').click()
-    driver.find_element('css selector', '#ab_options [role="menuitem"] a').click()
-    driver.wait_until(lambda: driver.find_elements('css selector', '#instant-radio > :last-child'))
-    driver.execute_script('window.scrollBy(0, 100000);')
-    driver.find_element('css selector', '#instant-radio > :last-child').click()
-    driver.find_element('css selector', '#form-buttons > :first-child').click()
-    driver.accept_alert()
-    driver.wait_until(lambda: init_results(driver))
-    assert_link_unchanged()
-
-    # Click the search result again
-    with driver.restore_url():
-        assert_no_intermediate_urls(driver, lambda: result.click(), href)
-    driver.wait_until(lambda: init_results(driver))
     assert_link_unchanged()
